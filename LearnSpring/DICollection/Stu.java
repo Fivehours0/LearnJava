ebaba80431279fe052d0bda662d83a712d148c9d
Fivehours0 <724642027@qq.com>
2020-11-04 21:38:36 +0800

    Collection

---
+++ LearnSpring/DICollection/Stu.java
@@ -0,0 +1,49 @@
+package LearnSpring.DICollection;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Stu {
+    private String[] course;
+    private List<Integer> list;
+    private Map<String, String> map;
+    private Set<Integer> set;
+    private List<Course> courseList;
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    public void setCourse(String[] course) {
+        this.course = course;
+    }
+
+    public String[] getCourse() {
+        return course;
+    }
+
+    public void setList(List<Integer> list) {
+        this.list = list;
+    }
+
+    public void setMap(Map<String, String> map) {
+        this.map = map;
+    }
+
+    public void setSet(Set<Integer> set) {
+        this.set = set;
+    }
+
+    @Override
+    public String toString() {
+        return "Stu{" +
+                "course=" + Arrays.toString(course) +
+                ", list=" + list +
+                ", map=" + map +
+                ", set=" + set +
+                ", course list=" + courseList +
+                '}';
+    }
+}
---
+++ LearnSpring/DICollection/Test.java
@@ -0,0 +1,27 @@
+package LearnSpring.DICollection;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+import java.util.Arrays;
+
+public class Test {
+    @org.junit.Test
+    public void test() {
+        ApplicationContext context = new FileSystemXmlApplicationContext("LearnSpring/DICollection/CollectionBean.xml");
+
+        Stu stu = context.getBean("stu", Stu.class);
+
+        System.out.println(stu.toString());
+    }
+
+    @org.junit.Test
+    public void publicBean() {
+        ApplicationContext context = new FileSystemXmlApplicationContext("LearnSpring/DICollection/publicBean.xml");
+
+        Stu stu = context.getBean("stu", Stu.class);
+
+        System.out.println(Arrays.toString(stu.getCourse()));
+
+    }
+}
