94509a945ce1865b95f0b326eff58514ace4fe61
Fivehours0 <724642027@qq.com>
2020-11-05 16:21:16 +0800

    AutoWrite

---
+++ LearnSpring/AutoWrite/Stu.java
@@ -0,0 +1,16 @@
+package LearnSpring.AutoWrite;
+
+public class Stu {
+    Course course;
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public String toString() {
+        return "Stu{" +
+                "course=" + course.toString() +
+                '}';
+    }
+}
---
+++ LearnSpring/AutoWrite/Test.java
@@ -0,0 +1,15 @@
+package LearnSpring.AutoWrite;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class Test {
+    @org.junit.Test
+    public void test() {
+        ApplicationContext context = new FileSystemXmlApplicationContext("LearnSpring/AutoWrite/AutoWrite.xml");
+
+        Stu stu = context.getBean("stu", Stu.class);
+
+        System.out.println(stu.toString());
+    }
+}
