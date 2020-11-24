6ac7384c61e20b060679a4a1e40c82f93d6bdcf2
Fivehours0 <724642027@qq.com>
2020-11-24 14:44:28 +0800

    init

--- DesignModel/VisitorPattern/ManyVisitor/Client.java
+++ DesignModel/VisitorPattern/ManyVisitor/Client.java
@@ -3,19 +3,19 @@ package DesignModel.VisitorPattern.ManyVisitor;
 /**
  * 多个观察者的情况
  */
-public class Client {
-    public static void main(String[] args) {
-        //展示报表访问者
-        IShowVisitor showVisitor = new ShowVisitor();
-        //汇总报表的访问者
-        ITotalVisitor totalVisitor = new TotalVisitor();
-        for(Employee emp:mockEmployee()){
-            emp.accept(showVisitor); //接受展示报表访问者
-            emp.accept(totalVisitor);//接受汇总表访问者
-        }
-        //展示报表
-        showVisitor.report();
-        //汇总报表
-        totalVisitor.totalSalary();
-    }
-}
+//public class Client {
+//    public static void main(String[] args) {
+//        //展示报表访问者
+//        IShowVisitor showVisitor = new ShowVisitor();
+//        //汇总报表的访问者
+//        ITotalVisitor totalVisitor = new TotalVisitor();
+//        for(Employee emp:mockEmployee()){
+//            emp.accept(showVisitor); //接受展示报表访问者
+//            emp.accept(totalVisitor);//接受汇总表访问者
+//        }
+//        //展示报表
+//        showVisitor.report();
+//        //汇总报表
+//        totalVisitor.totalSalary();
+//    }
+//}
