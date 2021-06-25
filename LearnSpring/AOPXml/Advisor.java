551731fc676f01e64d1c6981d930e9746e261d68
Fivehours0 <724642027@qq.com>
2021-06-25 21:04:15 +0800

    init

--- LearnSpring/AOPXml/Advisor.java
+++ LearnSpring/AOPXml/Advisor.java
@@ -1,7 +1,28 @@
 package AOPXml;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+
 public class Advisor {
     public void before() {
         System.out.println("before method");
+    }
+
+    public void afterReturning() {
+        System.out.println("afterReturning。。。。。。。。。。。。");
+    }
+
+    public void after() {
+        System.out.println("after最终通知。。。。。。。。。。。。");
+    }
+
+    public void around(ProceedingJoinPoint jp) throws Throwable {
+        System.out.println("around before。。。。。。。。。。。。");
+        jp.proceed();
+        System.out.println("around after。。。。。。。。。。。。");
+    }
+
+    public void afterThrowing() {
+        System.out.println("afterThrowing。。。。。。。。。。。。");
     }
 }
